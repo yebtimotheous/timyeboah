259b3d48f9cab8b4f013aff6cb11a37104e44f6e
TImothy Yeboah <31469829+yebtimotheous@users.noreply.github.com>
2024-12-13 19:44:25 +0000

    initial upload

---
+++ server.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "."
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped.")
+        httpd.shutdown() 
